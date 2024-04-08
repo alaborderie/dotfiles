332729751fbe8f06fdeb990f9ce8c7612587dbbe
Antoine Laborderie <antoine.laborderie@gmail.com>
2024-04-08 10:51:00 +0200

    :sparkles: theme change

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add onenord
+  { "rmehri01/onenord.nvim" },
+
+  -- Configure LazyVim to load onenord
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onenord",
+    },
+  },
+}
